4cb9bb9bb9d1e303af9dfac7a6ebafe4e16d39d9
ducanhdz <leanhthuongx7@gmail.com>
2024-12-11 23:18:55 +0700

    filemanager managaer product

---
+++ TH_Harmic/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TH_Harmic.Models;
+using TH_Harmic.Utilities;
+
+
+namespace Harmic.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly Th2Context  _context;
+
+        public ProductsController(Th2Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Products
+        public async Task<IActionResult> Index()
+        {
+            var harmicContext = _context.TbProducts.Include(t => t.CategoryProduct);
+            return View(await harmicContext.ToListAsync());
+        }
+
+        // GET: Admin/Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbProduct = await _context.TbProducts
+                .Include(t => t.CategoryProduct)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tbProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbProduct);
+        }
+
+        // GET: Admin/Products/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "Title");
+            return View();
+        }
+
+        // POST: Admin/Products/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Title,Alias,CategoryProductId,Description,Detail,Image,Price,PriceSale,Quantity,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,IsNew,IsBestSeller,UnitInStock,IsActive,Star")] TbProduct tbProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                tbProduct.Alias = TH_Harmic.Utilities.Function.TitleSlugGenerationAlias(tbProduct.Title);
+                _context.Add(tbProduct);
+                // lưu ngày tạo
+                tbProduct.CreatedDate = DateTime.Now;
+                // sản phảm thì chưa được chỉnh sủa
+                tbProduct.ModifiedDate = null;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "CategoryProductId");
+            return View(tbProduct);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbProduct = await _context.TbProducts.FindAsync(id);
+            if (tbProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "Title", tbProduct.CategoryProductId);
+            return View(tbProduct);
+        }
+
+        // POST: Admin/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Title,Alias,CategoryProductId,Description,Detail,Image,Price,PriceSale,Quantity,CreatedDate,CreatedBy,ModifiedDate,ModifiedBy,IsNew,IsBestSeller,UnitInStock,IsActive,Star")] TbProduct tbProduct)
+        {
+            if (id != tbProduct.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    tbProduct.ModifiedDate = DateTime.Now;
+                    _context.Update(tbProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TbProductExists(tbProduct.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryProductId"] = new SelectList(_context.TbProductCategories, "CategoryProductId", "CategoryProductId", tbProduct.CategoryProductId);
+            return View(tbProduct);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tbProduct = await _context.TbProducts
+                .Include(t => t.CategoryProduct)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tbProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tbProduct);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tbProduct = await _context.TbProducts.FindAsync(id);
+            if (tbProduct != null)
+            {
+                // tìm kiếm bản ghi bên bảng review có khoá ngoại liên kết với productID
+                var review = _context.TbProductReviews.Where(r => r.ProductId == id);
+                if (review.Any())
+                {
+                    _context.TbProductReviews.RemoveRange(review);
+                }
+                _context.TbProducts.Remove(tbProduct);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TbProductExists(int id)
+        {
+            return _context.TbProducts.Any(e => e.ProductId == id);
+        }
+    }
+}
